6183879ba36a0cb561fbfdf2b8762a6e7de6a43e
Guilherme Baron <floriani.guilherme@gmail.com>
2024-09-17 11:40:13 -0300

    feat(setup): initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use lexer::lexer;
+use winnow::{Located, Parser};
+
+mod lexer;
+mod types;
+
+fn main() {
+    let input = "3 - 1 /";
+    let tokens = lexer.parse(Located::new(input));
+    dbg!(tokens);
+}
